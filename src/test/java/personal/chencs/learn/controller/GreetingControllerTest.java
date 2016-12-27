c4615d2b26c875cfba0bf35c6e6bb97db616c846
chencs0603 <chencs0603@gmail.com>
2016-12-27 20:08:01 +0800

    解决Junit+Spring+Log4j的整合问题，即在启动Junit测试时，让Sprinng加载Log4j

--- src/test/java/personal/chencs/learn/controller/GreetingControllerTest.java
+++ src/test/java/personal/chencs/learn/controller/GreetingControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.ContextHierarchy;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -18,7 +17,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
 //XML风格
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(JUnit4ClassRunner.class)
 @WebAppConfiguration(value = "src/main/webapp")  
 //指定容器层次
 @ContextHierarchy({  
